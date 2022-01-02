637397ffcef36606a69e5fe87ab3992e07ab4d69
jose.estremadoyro <jeestremadoyro@gmail.com>
2022-01-02 17:24:47 -0600

    Added Give Consent and Collected Consents mocked pages

--- src/pages/Consents/index.js
+++ src/pages/Consents/index.js
@@ -1,20 +1,45 @@
 import Box from '@mui/material/Box';
 import logo from 'resources/logo.svg';
+import { DataGrid } from '@mui/x-data-grid';
+import './style.css';
+
+const Columns = [
+  { field: 'name', headerName: 'Name', width: 200 },
+  { field: 'email', headerName: 'Email', width: 200 },
+  { field: 'consent', headerName: 'Consent given for', width: 400 },
+];
+
+const Rows = [
+  {
+    id:'1',
+    name:'Example',
+    email:'ex@mp.le',
+    consent:'Receive newsletter',
+  },
+  {
+    id:'2',
+    name:'Example',
+    email:'ex@mp.le',
+    consent:'Receive newsletter',
+  },
+  {
+    id:'3',
+    name:'Example',
+    email:'ex@mp.le',
+    consent:'Receive newsletter',
+  }
+];
 
 const Consents = () => (
-  <Box className="MainLayout-header">
-    <img src={logo} className="MainLayout-logo" alt="logo" />
-    <p>
-      Edit <code>src/pages/Consents/index.js</code> and save to reload.
-    </p>
-    <a
-      className="MainLayout-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
+  <Box className="Consents" sx={{m:4}}>
+    <Box className="TableWrapper" sx={{m:4}}>
+      <DataGrid 
+        columns={Columns}
+        rows={Rows}
+        pageSize={2}
+        rowsPerPageOptions={[]}
+      /> 
+    </Box>
   </Box>
 );
 export default Consents;
--- src/pages/GiveConsent/index.js
+++ src/pages/GiveConsent/index.js
@@ -1,20 +1,39 @@
 import Box from '@mui/material/Box';
-import logo from 'resources/logo.svg';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import './style.css';
+
+const Consents = [
+  { label: 'Receive Newsletter' },
+  { label: 'Be shown targeted ads' },
+  { label: 'Contribute to anonymous visit statistics'}
+];
 
 const GiveConsent = () => (
-  <Box className="MainLayout-header">
-    <img src={logo} className="MainLayout-logo" alt="logo" />
-    <p>
-      Edit <code>src/pages/GiveConsent/index.js</code> and save to reload.
-    </p>
-    <a
-      className="MainLayout-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
+  <Box className="GiveConsent">
+    <Stack className="TextWrapper" spacing={2} direction="row" >
+      <TextField label="name" variant="outlined" />     
+      <TextField label="email" variant="outlined" />     
+    </Stack>
+    <Box sx={{my: 4}}> I agree to </Box>
+    <FormGroup sx={{border:'1px solid black', p:2, mb:4 }}>
+      { Consents.map(
+        ({label}) => (
+          <FormControlLabel
+            control={<Checkbox />}
+            label={label} 
+          />
+        )
+      )}
+    </FormGroup>
+    <Button variant="contained" size="large">
+      Give Consent 
+    </Button>
+
   </Box>
 );
 export default GiveConsent;
