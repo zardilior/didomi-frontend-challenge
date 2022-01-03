98c1ea718c829d9623c8b099fcc2b04a6ab11662
jose.estremadoyro <jeestremadoyro@gmail.com>
2022-01-03 12:57:37 -0600

    minor style changes

--- src/pages/Consents/index.js
+++ src/pages/Consents/index.js
@@ -1,45 +1,52 @@
+import { useState } from 'react';
 import Box from '@mui/material/Box';
-import logo from 'resources/logo.svg';
 import { DataGrid } from '@mui/x-data-grid';
+import { useQuery } from '@redux-requests/react';
 import './style.css';
 
 const Columns = [
-  { field: 'name', headerName: 'Name', width: 200 },
-  { field: 'email', headerName: 'Email', width: 200 },
-  { field: 'consent', headerName: 'Consent given for', width: 400 },
+  { field: 'name', headerName: 'Name', width: 100 },
+  { field: 'email', headerName: 'Email', width: 100 },
+  { field: 'consent', headerName: 'Consent given for', width: 200 },
 ];
 
-const Rows = [
-  {
-    id:'1',
-    name:'Example',
-    email:'ex@mp.le',
-    consent:'Receive newsletter',
-  },
-  {
-    id:'2',
-    name:'Example',
-    email:'ex@mp.le',
-    consent:'Receive newsletter',
-  },
-  {
-    id:'3',
-    name:'Example',
-    email:'ex@mp.le',
-    consent:'Receive newsletter',
-  }
-];
+const fetchConsents = (page) => ({
+  type: 'FETCH_CONSENTS',
+  request: { url: `/consents` },
+});
 
-const Consents = () => (
-  <Box className="Consents" sx={{m:4}}>
-    <Box className="TableWrapper" sx={{m:4}}>
-      <DataGrid 
-        columns={Columns}
-        rows={Rows}
-        pageSize={2}
-        rowsPerPageOptions={[]}
-      /> 
+const Consents = () => {
+  const [page,setPage] = useState(0);
+  const rowCount = 10;
+  const {
+    data: response, 
+    error,
+    loading 
+  } = useQuery({
+    action: fetchConsents,
+    variables: [page],
+    autoLoad: true,
+    autoReset: true
+  })
+
+  console.log({ response });
+  const data = response?.data || [];
+  const total = response?.total || 10;
+  return (
+    <Box className="Consents" sx={{m:4}}>
+      <Box className="TableWrapper" sx={{m:4}}>
+        <DataGrid 
+          columns={Columns}
+          rows={data} 
+          pageSize={2}
+          page={page}
+          rowCount={total}
+          onPageChange={setPage}
+          paginationMode="server"
+          rowsPerPageOptions={[]}
+        /> 
+      </Box>
     </Box>
-  </Box>
-);
+  );
+}
 export default Consents;
